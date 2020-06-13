3ce46841b29f6e98d248fae17ae52c1f941e9eb9
Sergey <35036709+sergiycheban@users.noreply.github.com>
2020-06-13 08:53:49 +0300

    Add files via upload

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -132,7 +132,6 @@ int main(int argc, char* argv[]) {
     Ray cam(Vec(50, 52, 295.6), Vec(0, -0.042612, -1).norm()); // cam pos, dir
     Vec cx = Vec(w * .5135 / h), cy = (cx % cam.d).norm() * .5135, r, * c = new Vec[w * h];
 
-    #pragma omp parallel for schedule(dynamic, 1) private(r)   
     for (int y = 0; y < h; y++) { // Loop over image rows
         fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samps * 4, 100. * y / (h - 1));
         for (unsigned short x = 0, Xi[3] = {
@@ -142,6 +141,8 @@ int main(int argc, char* argv[]) {
             }; x < w; x++) // Loop cols
             for (int sy = 0, i = (h - y - 1) * w + x; sy < 2; sy++) // 2x2 subpixel rows
                 for (int sx = 0; sx < 2; sx++, r = Vec()) { // 2x2 subpixel cols
+
+                    #pragma omp parallel for 
                     for (int s = 0; s < samps; s++) {
                         double r1 = 2 * erand48(Xi), dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
                         double r2 = 2 * erand48(Xi), dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
